a801b9106f3f5bb53163fcded4e16a735a8a9d4b
PedroHSiqueira <pedrohenriquedoamaralsiqueira@gmail.com>
2025-08-18 19:27:57 -0300

    Feat: Start Project

---
+++ src/main/java/dev/siqueira/financecontrol_sb/FinanceControlSbApplication.java
@@ -0,0 +1,13 @@
+package dev.siqueira.financecontrol_sb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceControlSbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinanceControlSbApplication.class, args);
+    }
+
+}
